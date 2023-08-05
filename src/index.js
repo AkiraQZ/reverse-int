28434a8d922455ef05372ad3fecc97d8828d11f6
AkiraQZ <vboyrkin@gmail.com>
2023-08-05 20:34:32 +0800

    feat: task comlited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    var arrReversed = [];
+    var nArray = n.toString().split('');
+    for (let index = nArray.length - 1; index >= 0; index--) {
+      arrReversed.push(nArray[index]);
+    }
+    return parseInt(arrReversed.join(''));
+  };
